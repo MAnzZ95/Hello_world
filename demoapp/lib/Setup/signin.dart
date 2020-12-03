2a115eb1e1d69ffadd91a32845d30d787f09b5ce
Supun Thilakarathne <Thilakarathnesupun@gmail.com>
2020-12-03 14:55:46 +0530

    check

---
+++ demoapp/lib/Setup/signin.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email, _password;
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sign in'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: [
+            TextFormField(
+              validator: (input) {
+                if (input.isEmpty) {
+                  return 'Please Enter valid Email';
+                }
+              },
+              onSaved: (input) => _email = input,
+              decoration: InputDecoration(labelText: 'Email'),
+            ),
+            TextFormField(
+              validator: (input) {
+                if (input.length < 6) {
+                  return 'Your password needs to be atleast 6 characters';
+                }
+              },
+              onSaved: (input) => _password = input,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            RaisedButton(
+              onPressed: () {},
+              child: Text('Sign in'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> signIn() async {
+    final formState = _formKey.currentState;
+    if (formState.validate()) {
+      formState.save();
+      try {
+        FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+      } catch (e) {
+        print(e.message);
+      }
+    }
+  }
+}
---
+++ demoapp/lib/pages/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+    );
+  }
+}
